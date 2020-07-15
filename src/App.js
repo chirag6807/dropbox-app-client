462a62b3e03e3bf98a0b59355eeb2bb50ca69228
chirag <radix@PC0413>
2020-07-15 15:23:44 +0530

    Task -176962 Remove Unwated file

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import LoginList from "./components/login.component";
 import SignUpComponent from "./components/sign-up.component";
 import HomeList from "./components/home";
-import CreateTodo from "./components/create-todo.component";
-import EditTodo from "./components/edit-todo.component";
 import ProfilePage from "./components/user-profile.component";
-// import logo from "./logo.png";
-
 
 function App() {
   return (
@@ -20,10 +16,6 @@ function App() {
           <Route path="/SignUp"  component={SignUpComponent} />
           <Route path="/file-store/:id" component={HomeList} />      
           <Route path="/profile" component={ProfilePage} />
-          {/* <Route path="/file-store/:id" component={FileList} /> */}
-          {/* <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} /> */}
-          {/* <Route path="/file-store" component={FileStoreList} /> */}
         </div>
     </Router>
   );
